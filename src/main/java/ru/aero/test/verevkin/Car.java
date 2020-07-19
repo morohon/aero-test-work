46533c54e9c2181eb6c6e67aa3c5b37d402074a3
Kirill Verevkin <kira795@yandex.ru>
2020-07-19 17:37:56 +0300

    add maven jar plugin, use multiply threads

--- src/main/java/ru/aero/test/verevkin/Car.java
+++ src/main/java/ru/aero/test/verevkin/Car.java
@@ -1,29 +1,25 @@
 package ru.aero.test.verevkin;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  * @author Kirill Verevkin
  */
 public class Car {
 
-    public static void main(String[] args) throws InterruptedException {
-        SpeedSensor ss = new SpeedSensor(0.8636f);
+    public static void main(String[] args) {
 
-        Random rand = new Random(1);
-        int r;
+        Thread[] threads = new Thread[4];
 
-        while (true){
-
-            ss.circle();
-            System.out.printf("current speed: %.3f / average speed: %.3f", ss.getCurrentSpeed() , ss.getAverageSpeed());
-            System.out.println();
-            r = rand.nextInt(10);
-            System.out.println("sleep: " + 100 * r);
-            Thread.sleep(100 * r);
+        for(int i = 0; i<=5;i++){
+            threads[i] = new Thread(() -> {
+                SpeedSensor ss = new SpeedSensor(0.8636f);
+                while (true) {
+                    ss.circle();
+                    System.out.printf(Thread.currentThread().getName()
+                            + ": current speed: %.3f / average speed: %.3f\n", ss.getCurrentSpeed(), ss.getAverageSpeed());
+                }
+            });
+            threads[i].start();
         }
-
     }
 
 }
